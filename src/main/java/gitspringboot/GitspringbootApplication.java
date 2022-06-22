bcc285bdc8d6e438a13b38cb663bd349063156ee
QSP <1013135308@qq.com>
2022-06-22 18:13:09 +0800

    上传项目

---
+++ src/main/java/gitspringboot/GitspringbootApplication.java
@@ -0,0 +1,13 @@
+package gitspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitspringbootApplication.class, args);
+    }
+
+}
