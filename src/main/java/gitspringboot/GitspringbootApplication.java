9cfa6c6a7e349dcc84dcbe5e613061e4704b7882
QSP <1013135308@qq.com>
2022-06-27 13:48:47 +0800

    修改Mybatis-plus启动报错

--- src/main/java/gitspringboot/GitspringbootApplication.java
+++ src/main/java/gitspringboot/GitspringbootApplication.java
@@ -1,9 +1,11 @@
 package gitspringboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("gitspringboot.mapper*")
 public class GitspringbootApplication {
 
     public static void main(String[] args) {
